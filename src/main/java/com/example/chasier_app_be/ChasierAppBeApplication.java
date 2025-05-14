f4a62a59b01e6449cd7b372a8cc738c110c69f01
auryno19 <aurynoaga19@gmail.com>
2025-05-14 08:22:31 +0700

    first commit

---
+++ src/main/java/com/example/chasier_app_be/ChasierAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.chasier_app_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChasierAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChasierAppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chasier_app_be/ChasierAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chasier_app_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChasierAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
